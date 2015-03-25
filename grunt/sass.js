a80df9f7a1f050e723328bbad1606ce35fc4410c
Erik Akers <erikakers@gmail.com>
2015-03-25 09:34:38 -0500

    task updates

--- grunt/sass.js
+++ grunt/sass.js
@@ -3,7 +3,7 @@ module.exports = {
     files: [{
         expand: true,
         cwd: '<%= paths.scss %>',
-        src: ['**/**/*.scss'],
+        src: ['*.scss'],
         dest: '<%= paths.dist %>/styles/',
         ext: '.css'
     }]
