acf0898280692d22ba8d8aa8b2899d912b0cec03
Erik Akers <erikakers@gmail.com>
2015-03-25 09:39:22 -0500

    updating grunt bump task

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,8 +1,17 @@
 module.exports = {
   options: {
-    files: ['package.json'],
-    commit: false,
-    createTag: false,
-    push: false
+    files: ['package.json', 'bower.json'],
+    commit: true,
+    commitMessage: 'Release v%VERSION%',
+    commitFiles: ['package.json', 'bower.json'],
+    createTag: true,
+    tagName: 'v%VERSION%',
+    tagMessage: 'Version %VERSION',
+    push: true,
+    pushTo: 'master',
+    gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+    globalReplace: false,
+    prereleaseName: false,
+    regExp: false
   }
 };
