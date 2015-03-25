579d6026210b9457da1f622dcf40d34d0324bc53
Erik Akers <erikakers@gmail.com>
2015-03-25 09:44:46 -0500

    adding bump tasks options

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,13 +1,13 @@
 module.exports = {
   options: {
     files: ['package.json', 'bower.json'],
-    commit: true,
+    commit: false,
     commitMessage: 'Release v%VERSION%',
     commitFiles: ['package.json', 'bower.json'],
-    createTag: true,
+    createTag: false,
     tagName: 'v%VERSION%',
     tagMessage: 'Version %VERSION',
-    push: true,
+    push: false,
     pushTo: 'master',
     gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
     globalReplace: false,
